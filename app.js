c5c7eb1ea31600411420f9a53b71381652205732
ronpe <ron.peretz1@gmail.com>
2021-08-15 12:10:32 +0300

    handle with search from users

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const configuration = require('./config');
 const imdbApi = require('./routers/imdbApi');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(imdbApi.router);
 const server = http.createServer(app);
 
 // get the 10 popular movie and then start the server
-imdbApi.initialPopularMovies()
+imdbApi.initialPopularMovies(true)
     .then(() => {
         server.listen(configuration.serverPort, () => {
             console.log(`Server start on port ${configuration.serverPort}`);
--- routers/imdbApi.js
+++ routers/imdbApi.js
@@ -2,22 +2,41 @@ const express = require('express');
 const fetch = require('node-fetch');
 const configuration = require('../config');
 
+
 const router = express.Router();
-const popularMoviesID = ['tt0241527', 'tt1201607', 'tt0094862', 'tt0111161', 'tt0167261', 'tt0120338'];
+const popularMoviesID = [{imdbID: 'tt0120591',}, {imdbID: 'tt1201607',}, {imdbID: 'tt0099253',},
+    {imdbID: 'tt0111161',}, {imdbID: 'tt0167261',}, {imdbID: 'tt0120338',}, {imdbID: 'tt0118158'}];
 let popularMoviesData = '';
 
-async function initialPopularMovies() {
-    const allPromises = popularMoviesID.map(movie => fetch(`https://www.omdbapi.com/?i=${movie}&apikey=${configuration.apiKey}`));
+async function getMoviesByID(isInitialMovies, searchMoviesList) {
+    const movies = isInitialMovies ? popularMoviesID : searchMoviesList;
+    const allPromises = movies.map(movie => fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${configuration.apiKey}`));
     const all = await Promise.all(allPromises);
-    const allAsJson = all.map(value => value.json());
-    popularMoviesData = await Promise.all(allAsJson);
+    const allMoviesAsJson = all.map(value => value.json());
+    const finalResults = await Promise.all(allMoviesAsJson);
+    if (isInitialMovies) {
+        popularMoviesData = finalResults;
+    }
+    return finalResults;
 }
 
-router.get('/getInitData', (req, res) => {
+router.get('/popularMovies', (req, res) => {
     res.status(200).send(popularMoviesData);
+});
+
+router.get('/searchMovie', (req, res) => {
+    const movieName = req.query.search;
+    fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=${configuration.apiKey}`)
+        .then(value => value.json())
+        .then(results => getMoviesByID(false, results.Search))
+        .then(results => {
+            res.status(200).send(results);
+        }).catch(error => {
+        res.status(501).send(JSON.stringify([]))
+    });
 });
 
 module.exports = {
     router,
-    initialPopularMovies
+    initialPopularMovies: getMoviesByID
 };
